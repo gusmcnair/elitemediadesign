55b38267bdbdc98edf0f8da413b8be6f8e91891e
Angus McNair <amcnair234@gmail.com>
2020-05-06 10:39:47 -0500

    Added insta API functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(".hamburger").click(function () {
 
 //Auto-closes mobile menu after a link has been clicked.
 $("li").click(function () {
-    if ($(window).width() < 800) {
+    if ($(window).width() < 700) {
         $("nav").removeClass(".show-menu").fadeOut(250);
     }
 });
@@ -19,28 +19,61 @@ $("li").click(function () {
 //(Fixes an issue in which after the hamburger menu was opened 
 //and closed, then changed to desktop size, menu would disappear)
 $(window).resize(function () {
-    if ($(window).width() >= 800) {
+    if ($(window).width() >= 700) {
         $("nav").show();
     } else ($("nav").hide())
 });
 
-    //Scrolls to mission on click of button
-    $(".mission-nav").click(function (){
-        $("html, body").animate({
-            scrollTop: $(".mission").offset().top
-        }, 1000)
-        })
+//Scrolls to mission on click of button
+$(".mission-nav").click(function () {
+    $("html, body").animate({
+        scrollTop: $(".mission").offset().top
+    }, 1000)
+})
 
-    //Scrolls to services on click of button
-    $(".services-nav").click(function (){
-        $("html, body").animate({
-            scrollTop: $(".services").offset().top
-        }, 1000)
-        })
+//Scrolls to services on click of button
+$(".services-nav").click(function () {
+    $("html, body").animate({
+        scrollTop: $(".services").offset().top
+    }, 1000)
+})
 
-    //Scrolls to contact on click of button
-    $(".contact-nav").click(function (){
-        $("html, body").animate({
-            scrollTop: $(".contact-form").offset().top
-        }, 1000)
+//Scrolls to contact on click of button
+$(".contact-nav").click(function () {
+    $("html, body").animate({
+        scrollTop: $(".contact-form").offset().top
+    }, 1000)
+})
+
+function getInsta() {
+    fetch('https://www.instagram.com/goblue42agency/?__a=1')
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
         })
+        .then(response => {
+            handleResponse(response)
+        })
+        .catch(err => handleError(err))
+
+}
+
+function handleResponse(response) {
+    console.log(response)
+    const media = response.graphql.user.edge_owner_to_timeline_media.edges
+    $('.description').append(response.graphql.user.biography)
+    for (let i = 0; i < media.length; i++) {
+        const url = media[i].node.display_url
+        const image = document.createElement('IMG')
+        image.src = url
+        $('.posts').append(image)
+    }
+}
+
+function handleError(err) {
+    $('.posts').append('<p class=error>Instagram feed could not be loaded.</p>')
+}
+
+
+$(document).ready(getInsta)
